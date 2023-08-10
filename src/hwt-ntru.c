c99a9dfdf554d9bb95af0e27d8672714f08b67f4
hyoeun <she000@cryptolab.co.kr>
2023-08-10 20:20:57 +0900

    Update NTRU to round3 code

--- src/hwt-ntru.c
+++ src/hwt-ntru.c
@@ -1,43 +1,20 @@
 #include "hwt.h"
 
+#define int32 int32_t
+
 #define int32_MINMAX(a, b)                                                     \
     do {                                                                       \
-        int32_t ab = b ^ a;                                                    \
-        int32_t c = b - a;                                                     \
-        c ^= ab & (c ^ b);                                                     \
+        int32_t ab = (b) ^ (a);                                                \
+        int32_t c = (int32_t)((int64_t)(b) - (int64_t)(a));                    \
+        c ^= ab & (c ^ (b));                                                   \
         c >>= 31;                                                              \
         c &= ab;                                                               \
-        a ^= c;                                                                \
-        b ^= c;                                                                \
+        (a) ^= c;                                                              \
+        (b) ^= c;                                                              \
     } while (0)
 
-void crypto_sort(void *array, long long n) {
-    long long top, p, q, r, i;
-    int32_t *x = array;
-
-    if (n < 2)
-        return;
-    top = 1;
-    while (top < n - top)
-        top += top;
-
-    for (p = top; p > 0; p >>= 1) {
-        for (i = 0; i < n - p; ++i)
-            if (!(i & p))
-                int32_MINMAX(x[i], x[i + p]);
-        i = 0;
-        for (q = top; q > p; q >>= 1) {
-            for (; i < n - q; ++i) {
-                if (!(i & p)) {
-                    int32_t a = x[i + p];
-                    for (r = q; r > p; r >>= 1)
-                        int32_MINMAX(a, x[i + r]);
-                    x[i + p] = a;
-                }
-            }
-        }
-    }
-}
+void crypto_sort_int32(int32 *array, size_t n);
+void crypto_sort(void *array, long long n);
 
 void sample_fixed_type(uint8_t *res, uint8_t *cnt_arr, const uint8_t *input,
                        const size_t input_size, const uint16_t hmwt) {
@@ -69,7 +46,8 @@ void sample_fixed_type(uint8_t *res, uint8_t *cnt_arr, const uint8_t *input,
     for (i = hmwt / 2; i < hmwt; i++)
         s[i] |= 2;
 
-    crypto_sort(s, DIMENSION);
+    // crypto_sort(s, DIMENSION);
+    crypto_sort_int32(s, DIMENSION);
 
     for (i = 0; i < DIMENSION; i++)
         res[i] = ((uint16_t)(s[i] & 3));
@@ -78,5 +56,99 @@ void sample_fixed_type(uint8_t *res, uint8_t *cnt_arr, const uint8_t *input,
     for (i = 0; i < DIMENSION; ++i) {
         cnt_arr_idx = ((i & 0x700) >> 8) & (-(res[i] & 0x01));
         cnt_arr[cnt_arr_idx] += (res[i] & 0x01);
+    }
+}
+
+/* assume 2 <= n <= 0x40000000 */
+void crypto_sort_int32(int32 *array, size_t n) {
+    size_t top, p, q, r, i, j;
+    int32 *x = array;
+
+    top = 1;
+    while (top < n - top)
+        top += top;
+
+    for (p = top; p >= 1; p >>= 1) {
+        i = 0;
+        while (i + 2 * p <= n) {
+            for (j = i; j < i + p; ++j) {
+                int32_MINMAX(x[j], x[j + p]);
+            }
+            i += 2 * p;
+        }
+        for (j = i; j < n - p; ++j) {
+            int32_MINMAX(x[j], x[j + p]);
+        }
+
+        i = 0;
+        j = 0;
+        for (q = top; q > p; q >>= 1) {
+            if (j != i) {
+                for (;;) {
+                    if (j == n - q)
+                        goto done;
+                    int32 a = x[j + p];
+                    for (r = q; r > p; r >>= 1) {
+                        int32_MINMAX(a, x[j + r]);
+                    }
+                    x[j + p] = a;
+                    ++j;
+                    if (j == i + p) {
+                        i += 2 * p;
+                        break;
+                    }
+                }
+            }
+            while (i + p <= n - q) {
+                for (j = i; j < i + p; ++j) {
+                    int32 a = x[j + p];
+                    for (r = q; r > p; r >>= 1) {
+                        int32_MINMAX(a, x[j + r]);
+                    }
+                    x[j + p] = a;
+                }
+                i += 2 * p;
+            }
+            /* now i + p > n - q */
+            j = i;
+            while (j < n - q) {
+                int32 a = x[j + p];
+                for (r = q; r > p; r >>= 1) {
+                    int32_MINMAX(a, x[j + r]);
+                }
+                x[j + p] = a;
+                ++j;
+            }
+
+        done:;
+        }
+    }
+}
+
+void crypto_sort(void *array, long long n) {
+    long long top, p, q, r, i;
+    int32_t *x = array;
+
+    if (n < 2)
+        return;
+    top = 1;
+    while (top < n - top)
+        top += top;
+
+    for (p = top; p > 0; p >>= 1) {
+        for (i = 0; i < n - p; ++i)
+            if (!(i & p))
+                int32_MINMAX(x[i], x[i + p]);
+        i = 0;
+        for (q = top; q > p; q >>= 1) {
+            for (; i < n - q; ++i) {
+                if (!(i & p)) {
+                    int32_t a = x[i + p];
+                    for (r = q; r > p; r >>= 1)
+                        int32_MINMAX(a, x[i + r]);
+                    x[i + p] = a;
+                }
+            }
+        }
     }
 }
