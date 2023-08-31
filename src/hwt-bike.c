cc2c24eb37afcb02be7029d6c957c84d8d01802f
hyoeun <she000@cryptolab.co.kr>
2023-08-31 16:57:17 +0900

    Minor update (deprecate unusual APIs)

--- src/hwt-bike.c
+++ src/hwt-bike.c
@@ -136,37 +136,3 @@ void hwt_bike_degree_test(uint64_t *deg_dist, uint64_t *cnt_dist,
     //     cnt_dist[cnt_arr_idx] += (res[i] & 0x01);
     // }
 }
-
-void hwt_bike_test(uint8_t *res, uint8_t *cnt_arr, const uint8_t *input,
-                   const size_t input_size, const uint16_t hmwt) {
-
-    uint32_t i, pos = 0;
-    uint32_t deg = 0;
-    uint32_t buf[SHAKE256_RATE * 2] = {0};
-
-    keccak_state state;
-    shake256_init(&state);
-    shake256_absorb_once(&state, input, input_size);
-    shake256_squeezeblocks((uint8_t *)buf, 8, &state);
-
-    for (i = 0; i < DIMENSION; ++i)
-        res[i] = 0;
-
-    for (i = DIMENSION - hmwt; i < DIMENSION; ++i) {
-        uint64_t rand = (uint64_t)buf[pos] * (i + 1);
-        deg = (uint32_t)(rand >> 32);
-        printf("%ld ", deg);
-
-        // res[i] = res[deg];
-        // res[deg] =
-        //     ((*((uint8_t *)buf + sizeof(uint32_t) * hmwt + pos)) & 0x02) - 1;
-        pos++;
-    }
-    printf("\n");
-
-    // size_t cnt_arr_idx = 0;
-    // for (i = 0; i < DIMENSION; ++i) {
-    //     cnt_arr_idx = ((i & 0x700) >> 8) & (-(res[i] & 0x01));
-    //     cnt_arr[cnt_arr_idx] += (res[i] & 0x01);
-    // }
-}
